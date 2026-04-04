025ebcc6c3dc1929466eb7095979d9930be090ea
Jan-Hendrik H <hoeltkemeier@campus.tu-berlin.de>
2026-04-04 23:24:21 +0200

    retrieval and extraction logic (not tested)

--- scripts/extract_kpi.py
+++ scripts/extract_kpi.py
@@ -37,3 +37,94 @@ def call_ollama_extraction(prompt):
         print(f"Error: {e}")
         return None
     
+
+def main():
+    #Since the vector database is PDF specific, vector storage is ephemeral (in-memory vector database)
+    embedding_function = HuggingFaceEmbeddingFunction(model_name=EMBED_MODEL_ID)
+    vector_storage = chromadb.EphemeralClient()
+
+    if not METADATA_PATH.exists():
+        print("The metadata path does not exist")
+        return
+    
+    #load metadata json document
+    with METADATA_PATH.open("f", encoding="utf-8") as f:
+        metadata = json.load(f)
+
+    #list all chunks (never between two companies)
+    chunk_files = list(CHUNKS_DIR.glob("*_chunks.json"))
+    if not chunk_files:
+        print("Error: There are no chunks to process.")
+        return
+    
+    #loop over chunk files
+    for chunk_file in chunk_files:
+        company_id = chunk_file.stem.replace("_chunks", "")
+        print(f"Processing report from {company_id}")
+
+        collection_name=f"idx_{company_id}"
+        collection = vector_storage.create_collection(
+            name=collection_name,
+            embedding_function=embedding_function
+        )
+
+        with chunk_file.open("r", encoding="utf-8") as f:
+            report_chunks=json.load(f)
+
+        """
+        Syntax for collection.add: 
+        collection.add(
+        ids=["id1", "id2", "id3"],
+        documents=["lorem ipsum...", "doc2", "doc3"],
+        metadatas=[{"chapter": 3, "verse": 16}, {"chapter": 3, "verse": 5}, {"chapter": 29, "verse": 11}],
+        )
+        """
+
+        collection.add(
+            ids=[c["id"]for c in report_chunks],
+            documents=[c["text"]for c in report_chunks],
+            metadatas=[c["metadata"]for c in report_chunks]
+        )
+
+        extraction_results = {}
+
+        #loop over every kpi within the company
+        for kpi_key, kpi_info in metadata["KPIs"].items():
+            print(f"Extracting {kpi_info["name"]}")
+
+            query_results=collection.query(
+                query_texts=[kpi_info["search_string"]],
+                n_results=10
+            )
+
+            context_text ="\n---\n".join(query_results["documents"][0])
+
+            prompt=f""" 
+            Extract the following KPI from the sustainability report context provided below.
+            
+            KPI Target: {kpi_info['name']}
+            Technical Definition: {kpi_info['definition']}
+            Target Unit: {kpi_info['unit']}
+            Calculation Logic: {kpi_info.get('calculation_logic', 'Direct extraction')}
+            
+            Glossary Reference:
+            {json.dumps(metadata.get('glossary', {}), indent=2)}
+
+            Sustainability Report Context:
+            {context_text}
+            """
+
+            results = call_ollama_extraction(prompt)
+            if results:
+                extraction_results[kpi_key]=results.model_dump()
+
+        
+        output_path = RESULTS_DIR / f"{company_id}_extracted.json"
+        with output_path.open("w", encoding="utf-8") as f:
+            json.dump(extraction_results, f, indent=2)
+
+        print("Success. Data exported to {output_path}")
+
+
+if __name__ == "__main__":
+    main()
