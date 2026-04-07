8216f68bf4c1859af2c8eb3fc31b3ffe7e47cb9c
Jan-Hendrik H <hoeltkemeier@campus.tu-berlin.de>
2026-04-07 20:58:55 +0200

    Implemented PDFPipelineOptions from Docling. Testing not possible, because heavy on RAM. TBD: Split PDF with pypdf

--- scripts/parse_reports.py
+++ scripts/parse_reports.py
@@ -1,9 +1,15 @@
 import json
 from pathlib import Path
-from docling.document_converter import DocumentConverter
+from docling.document_converter import DocumentConverter, InputFormat, PdfFormatOption
+from docling.datamodel.pipeline_options import(
+    PdfPipelineOptions, TableStructureOptions, TableFormerMode, granite_picture_description)
+import gc #garbage collection
+from pypdf import PdfReader, PdfWriter #splitting pdf for better memory usage
+
 
 #This script looks for sustainability reports in data/raw , converts with Docling into a JSON format to be later converted into DoclingDocument, and then pasts the results into data/output.
 #1.Path definition, 2.Iterate reports and convert with Docling (skip if target JSON/Markdown already exists)
+
 
 def main():
     #(1)
@@ -11,8 +17,28 @@ def main():
     output_dir = Path("data/processed")
     output_dir.mkdir(parents=True, exist_ok=True)
 
+    #Change: PDFPipelineOptions() (specifically designed for procesisng PDFs and image-rich documents)
+    pipeline_options = PdfPipelineOptions()
+    pipeline_options.do_table_structure = True
+    pipeline_options.table_structure_options.mode = TableFormerMode.ACCURATE
+
+    #Picture description with Doclings PDF_PipelineOptions()
+    pipeline_options.do_picture_description = True
+    #for now: using pre defined granite-vision-3.1-2b-preview from IBM
+    pipeline_options.picture_description_options = (granite_picture_description)
+    pipeline_options.picture_description_options.prompt = (
+    "Describe the image as clearly as possible. In the process of doing so, name every KPI that is mentioned in the picture and all necessary information to classify that KPI. Be accurate."
+    )
+    pipeline_options.images_scale = 0.6
+    pipeline_options.generate_picture_images = False
+
+    #For scanned reports:
+    #pipeline_options.do_ocr = True
+
     print("Initializing Docling...")
-    converter = DocumentConverter()
+    converter = DocumentConverter(
+        format_options={InputFormat.PDF: PdfFormatOption(pipeline_options = pipeline_options)}
+        )
 
     pdf_files = list(input_dir.glob("*.pdf"))
 
@@ -39,10 +65,12 @@ def main():
                 
             print(f"Success: Saved to {output_path.name}")
 
+            del result
+            gc.collect
 
         except Exception as e:
             print(f"Failed to process {pdf_path.name}: {e}")
-            
+            gc.collect
 
 
 
